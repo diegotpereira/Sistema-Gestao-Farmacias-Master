747f27f4fffad26430c2d50e5b406d3c9594ea65
T.I <T.I@TI.rsatacadao.local>
2020-10-05 12:22:48 -0300

    criação do arquivo classe Conexao.java

---
+++ Sistema-Gestao-Farmacias-Master/src/br/com/java/connection/Conexao.java
@@ -0,0 +1,12 @@
+package br.com.java.connection;
+
+import java.sql.Connection;
+
+public class Conexao {
+
+	public static Connection getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
