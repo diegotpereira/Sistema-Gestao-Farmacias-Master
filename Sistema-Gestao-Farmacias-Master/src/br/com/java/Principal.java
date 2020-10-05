24b02c9baf1d1817c1bd4af28fd5510305898226
Diego <diegoteixeirapereira@hotmail.com>
2020-10-05 10:32:42 -0300

    alteração no arquivo classe principal adicionado metodo chamar login.java

--- Sistema-Gestao-Farmacias-Master/src/br/com/java/Principal.java
+++ Sistema-Gestao-Farmacias-Master/src/br/com/java/Principal.java
@@ -1,9 +1,12 @@
 package br.com.java;
 
+import br.com.java.auth.Login;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		new Login().setVisible(true);
 
 	}
 
