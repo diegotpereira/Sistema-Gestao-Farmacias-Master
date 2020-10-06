7680d4e4cdec73e278c019d8f50209a64aae83ed
Diego <diegoteixeirapereira@hotmail.com>
2020-10-06 09:26:30 -0300

    alteração no arquivo classe Drug adicionados componentes

--- Sistema-Gestao-Farmacias-Master/src/br/com/java/view/Drug.java
+++ Sistema-Gestao-Farmacias-Master/src/br/com/java/view/Drug.java
@@ -1,5 +1,9 @@
 package br.com.java.view;
 
-public class Drug {
+public class Drug extends javax.swing.JFrame{
+	
+	public javax.swing.JButton adddrug;
+	public javax.swing.JButton deletedrug;
+	public javax.swing.JButton updatedrug;
 
 }
