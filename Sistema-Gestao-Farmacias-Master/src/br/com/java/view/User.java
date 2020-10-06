f28bc60c31d521cbc8ebe4d46922d969d00236e8
Diego <diegoteixeirapereira@hotmail.com>
2020-10-06 09:26:06 -0300

    criação do arquivo classe e formulário User

---
+++ Sistema-Gestao-Farmacias-Master/src/br/com/java/view/User.java
@@ -0,0 +1,9 @@
+package br.com.java.view;
+
+public class User extends javax.swing.JFrame {
+	
+	public javax.swing.JButton adduser;
+	public javax.swing.JButton deleteuser;
+	public javax.swing.JButton updateuser;
+
+}
