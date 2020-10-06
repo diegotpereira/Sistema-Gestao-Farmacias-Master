83dc8b83845b3d7d368b74021885d9a861624fa3
Diego <diegoteixeirapereira@hotmail.com>
2020-10-06 09:57:17 -0300

    alteração no arquivo classe User.java adicionado componente

--- Sistema-Gestao-Farmacias-Master/src/br/com/java/view/User.java
+++ Sistema-Gestao-Farmacias-Master/src/br/com/java/view/User.java
@@ -5,5 +5,6 @@ public class User extends javax.swing.JFrame {
 	public javax.swing.JButton adduser;
 	public javax.swing.JButton deleteuser;
 	public javax.swing.JButton updateuser;
+	public static javax.swing.JTextField id;
 
 }
