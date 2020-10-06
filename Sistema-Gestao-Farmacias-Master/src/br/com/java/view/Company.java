b16263ddf7328e838a188c5fe94c9859762cdc5f
Diego <diegoteixeirapereira@hotmail.com>
2020-10-06 09:18:26 -0300

    alteração no arquivo classe Company adicionado componentes

--- Sistema-Gestao-Farmacias-Master/src/br/com/java/view/Company.java
+++ Sistema-Gestao-Farmacias-Master/src/br/com/java/view/Company.java
@@ -1,5 +1,9 @@
 package br.com.java.view;
 
 public class Company extends javax.swing.JFrame{
+	
+	public javax.swing.JButton save;
+	public javax.swing.JButton update;
+	public javax.swing.JButton delete;
 
 }
