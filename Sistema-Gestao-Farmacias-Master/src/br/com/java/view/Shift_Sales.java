ea0ffe661604219cc8b5ad9ac0d42b10840dbc86
T.I <T.I@TI.rsatacadao.local>
2020-10-15 13:35:31 -0300

    alteração

--- Sistema-Gestao-Farmacias-Master/src/br/com/java/view/Shift_Sales.java
+++ Sistema-Gestao-Farmacias-Master/src/br/com/java/view/Shift_Sales.java
@@ -195,7 +195,7 @@ public class Shift_Sales extends javax.swing.JFrame{
         jPanel5.setBorder(javax.swing.BorderFactory.createMatteBorder(1, 1, 1, 1, new java.awt.Color(255, 255, 255)));
 
         total.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
-        total.setText("R$ 00.0");
+        total.setText("00.0$");
         total.setForeground(new java.awt.Color(255, 255, 255));
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
@@ -372,7 +372,13 @@ public class Shift_Sales extends javax.swing.JFrame{
 		           
 				amount += Double.parseDouble(res.getString("AMOUNT")) ;
 		       
+<<<<<<< HEAD
 			} total.setText(String.valueOf(amount)+"R$"); //help
+=======
+			} 
+			
+			total.setText(String.valueOf(amount)+"$");
+>>>>>>> refs/remotes/origin/master
 		       
 		} catch (Exception e) {
 		        
@@ -405,6 +411,7 @@ public class Shift_Sales extends javax.swing.JFrame{
         
 		if(day.getSelectedIndex()==0&&month.getSelectedIndex()==0&&year.getSelectedIndex()==0){
 
+<<<<<<< HEAD
 			
 			sql = "select USER_NAME,BARCODE,NAME,QUANTITY,PRICE,AMOUNT,DATE from history_sales where USER_NAME='"+username.getSelectedItem()+"' ";
        
@@ -428,6 +435,31 @@ public class Shift_Sales extends javax.swing.JFrame{
 			
 		}else {
 
+
+			sql = "select USER_NAME,BARCODE,NAME,QUANTITY,PRICE,AMOUNT,DATE from history_sales where USER_NAME='"+username.getSelectedItem()+"' ";
+       
+			double amount = 0; 
+       
+			try {
+       
+				pre=con.prepareStatement(sql);
+       
+				res=pre.executeQuery();
+       
+				jTable1.setModel(DbUtils.resultSetToTableModel(res));
+       
+				help_user1();	
+       
+			} catch (Exception e) {
+        
+				JOptionPane.showMessageDialog(null,e.getMessage(),"Error",2);
+        
+			}
+			
+		}else {
+
+			
+>>>>>>> refs/remotes/origin/master
 			sql = "select USER_NAME,BARCODE,NAME,QUANTITY,PRICE,AMOUNT,DATE from history_sales where USER_NAME='"+username.getSelectedItem()+"' and DATE='"+day.getSelectedItem()+"-"+month.getSelectedItem()+"-"+year.getSelectedItem()+"' ";
        
 			double amount = 0; 
